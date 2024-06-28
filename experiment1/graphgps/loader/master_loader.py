630cda09cc74c0bfd1085250ffd451c0148ca5b1
schwollie <lars@familiechristiansen.de>
2024-06-28 15:32:23 +0200

    add gitignore and basic multiple virtual nodes
    fix to large data
    
    remove data

--- experiment1/graphgps/loader/master_loader.py
+++ experiment1/graphgps/loader/master_loader.py
@@ -179,6 +179,10 @@ def load_dataset_master(format, name, dataset_dir):
             dataset = preformat_Peptides(dataset_dir, name[2:], vn=False)
         elif name.startswith('PureVN-peptides-'):
             dataset = preformat_Peptides(dataset_dir, name[7:], vn=False, purevn=True)
+        elif name.startswith('MVN-peptides-'): # multiple virtual nodes
+            dataset = preformat_Peptides(dataset_dir, name[4:], mvn=True)
+        elif name.startswith('HSG-peptides-'): # Hierarchical Support Graphs
+            dataset = preformat_Peptides(dataset_dir, name[4:], hsg=True)
 
         ### Link prediction datasets.
         elif name.startswith('ogbl-'):
@@ -498,7 +502,6 @@ class VirtualNode(BaseTransform):
     Furthermore, special edge types will be added both for in-coming and
     out-going information to and from the virtual node.
     """
-
     def __call__(self, data: Data) -> Data:
         num_nodes, (row, col) = data.num_nodes, data.edge_index
         edge_type = data.get('edge_type', torch.zeros_like(row))
@@ -546,6 +549,139 @@ class VirtualNode(BaseTransform):
             data.num_nodes = old_data.num_nodes + 1
         return data
 
+# [docs]@functional_transform('virtual_nodes')
+class MultipleVirtualNodes(BaseTransform):
+    r"""Appends multiple virtual nodes to the given homogeneous graph that is connected
+    to all other nodes, as described in the `"Neural Message Passing for
+    Quantum Chemistry" <https://arxiv.org/abs/1704.01212>`_ paper
+    (functional name: :obj:`virtual_node`).
+    The virtual node serves as a global scratch space that each node both reads
+    from and writes to in every step of message passing.
+    This allows information to travel long distances during the propagation
+    phase.
+
+    Node and edge features of the virtual node are added as zero-filled input
+    features.
+    Furthermore, special edge types will be added both for in-coming and
+    out-going information to and from the virtual node.
+    """
+    def __init__(self, num_vn: int = 10):
+        self.num_vn = num_vn
+
+    def __call__(self, data: Data) -> Data:
+        num_nodes, (row, col) = data.num_nodes, data.edge_index
+        edge_type = data.get('edge_type', torch.zeros_like(row))
+
+        arange = torch.arange(num_nodes, device=row.device)
+        full = row.new_full((num_nodes,), num_nodes)
+        row = torch.cat([row, arange, full], dim=0)
+        col = torch.cat([col, full, arange], dim=0)
+        edge_index = torch.stack([row, col], dim=0)
+
+        new_type = edge_type.new_full((num_nodes,), int(edge_type.max()) + 1)
+        edge_type = torch.cat([edge_type, new_type, new_type + 1], dim=0)
+
+        #new_type = edge_type.new_full((num_nodes * self.num_vn,), int(edge_type.max()) + 1)
+        # Concatenating edge types (`edge_type` update)
+        #edge_type = torch.cat([edge_type, new_type.view(-1, self.num_vn), new_type.view(-1, self.num_vn) + 1], dim=0)
+
+        old_data = copy.copy(data)
+        for key, value in old_data.items():
+            if key == 'edge_index' or key == 'edge_type':
+                continue
+
+            if isinstance(value, Tensor):
+                dim = old_data.__cat_dim__(key, value)
+                size = list(value.size())
+
+                fill_value = None
+                if key == 'edge_weight':
+                    size[dim] = (1+self.num_vn) * num_nodes
+                    fill_value = 1.
+                elif key == 'batch':
+                    size[dim] = 1
+                    fill_value = int(value[0])
+                elif old_data.is_edge_attr(key):
+                    size[dim] = (1+self.num_vn) * num_nodes
+                    fill_value = 0.
+                elif old_data.is_node_attr(key):
+                    size[dim] = 1
+                    fill_value = 0.
+
+                if fill_value is not None:
+                    new_value = value.new_full(size, fill_value)
+                    data[key] = torch.cat([value, new_value], dim=dim)
+
+        data.edge_index = edge_index
+        data.edge_type = edge_type
+
+        if 'num_nodes' in data:
+            data.num_nodes = old_data.num_nodes + self.num_vn
+        return data
+
+# [docs]@functional_transform('virtual_nodes')
+class HierarchicalSupportGraph(BaseTransform):
+    r"""Appends multiple virtual nodes as a HierarchicalSupportGraph to the given homogeneous graph that is connected
+    to all other nodes, as described in the `"Neural Message Passing for
+    Quantum Chemistry" <https://arxiv.org/abs/1704.01212>`_ paper
+    (functional name: :obj:`virtual_node`).
+    The virtual node serves as a global scratch space that each node both reads
+    from and writes to in every step of message passing.
+    This allows information to travel long distances during the propagation
+    phase.
+
+    Node and edge features of the virtual node are added as zero-filled input
+    features.
+    Furthermore, special edge types will be added both for in-coming and
+    out-going information to and from the virtual node.
+    """
+
+    def __call__(self, data: Data) -> Data:
+        num_nodes, (row, col) = data.num_nodes, data.edge_index
+        edge_type = data.get('edge_type', torch.zeros_like(row))
+
+        arange = torch.arange(num_nodes, device=row.device)
+        full = row.new_full((num_nodes,), num_nodes)
+        row = torch.cat([row, arange, full], dim=0)
+        col = torch.cat([col, full, arange], dim=0)
+        edge_index = torch.stack([row, col], dim=0)
+
+        new_type = edge_type.new_full((num_nodes,), int(edge_type.max()) + 1)
+        edge_type = torch.cat([edge_type, new_type, new_type + 1], dim=0)
+
+        old_data = copy.copy(data)
+        for key, value in old_data.items():
+            if key == 'edge_index' or key == 'edge_type':
+                continue
+
+            if isinstance(value, Tensor):
+                dim = old_data.__cat_dim__(key, value)
+                size = list(value.size())
+
+                fill_value = None
+                if key == 'edge_weight':
+                    size[dim] = 2 * num_nodes
+                    fill_value = 1.
+                elif key == 'batch':
+                    size[dim] = 1
+                    fill_value = int(value[0])
+                elif old_data.is_edge_attr(key):
+                    size[dim] = 2 * num_nodes
+                    fill_value = 0.
+                elif old_data.is_node_attr(key):
+                    size[dim] = 1
+                    fill_value = 0.
+
+                if fill_value is not None:
+                    new_value = value.new_full(size, fill_value)
+                    data[key] = torch.cat([value, new_value], dim=dim)
+
+        data.edge_index = edge_index
+        data.edge_type = edge_type
+
+        if 'num_nodes' in data:
+            data.num_nodes = old_data.num_nodes + 1
+        return data
 
 # [docs]@functional_transform('virtual_node')
 class PureVirtualNode(BaseTransform):
@@ -617,7 +753,7 @@ class FakeEdgeFeatureTransform(BaseTransform):
             return data
 
 
-def preformat_Peptides(dataset_dir, name, vn=False, purevn=False):
+def preformat_Peptides(dataset_dir, name, vn=False, purevn=False, mvn=False, hsg=False):
     """Load Peptides dataset, functional or structural.
 
     Note: This dataset requires RDKit dependency!
@@ -646,6 +782,10 @@ def preformat_Peptides(dataset_dir, name, vn=False, purevn=False):
     transform = {'transform': None if vn == False else VirtualNode()}
     if purevn:
         transform['transform'] = PureVirtualNode()
+    if mvn:
+        transform['transform'] = MultipleVirtualNodes()
+    if hsg:
+        transform['transform'] = HierarchicalSupportGraph()
 
     if dataset_type == 'functional':
         dataset = PeptidesFunctionalDataset(dataset_dir, **transform)
@@ -787,8 +927,9 @@ def join_dataset_splits(datasets):
 
 if __name__ == '__main__':
     # dataset = TUDataset('/tmp', 'DD', pre_transform=VirtualNode())
-    dataset = preformat_VOCSuperpixels('/tmp', 'edge_wt_region_boundary', 30, vn=False)
-    dataset_vn = preformat_VOCSuperpixels('/tmp', 'edge_wt_region_boundary', 30, vn=True)
-    import pdb;
+    #dataset = preformat_VOCSuperpixels('/tmp', 'edge_wt_region_boundary', 30, vn=False)
+    #dataset_vn = preformat_VOCSuperpixels('/tmp', 'edge_wt_region_boundary', 30, vn=True)
+    #import pdb;
+    #pdb.set_trace()
 
-    pdb.set_trace()
+    preformat_Peptides('/dataset', 'edge_wt_region_boundary', 30, vn=True)
