a958630ef2d646e633c090f0624267983739bc38
schwollie <lars@familiechristiansen.de>
2024-07-04 15:32:27 +0200

    update runAll shell script

--- experiment1/runExtension.sh
+++ experiment1/runExtension.sh
@@ -2,18 +2,31 @@
 
 conda activate env_paper_20
 
-#MVN
-python main.py --cfg configs/GatedGCN/MVN-peptides-func-GatedGCN.yaml  wandb.use False
-python main.py --cfg configs/GatedGCN/MVN-peptides-func-GatedGCN+RWSE.yaml  wandb.use False
+#MVN 5 nodes
+python main.py --cfg configs/GatedGCN/MVN5-peptides-func-GatedGCN.yaml  wandb.use False
+python main.py --cfg configs/GatedGCN/MVN5-peptides-func-GatedGCN+RWSE.yaml  wandb.use False
 
-python main.py --cfg configs/GatedGCN/MVN-peptides-struct-GatedGCN.yaml  wandb.use False
-python main.py --cfg configs/GatedGCN/MVN-peptides-struct-GatedGCN+RWSE.yaml  wandb.use False
+python main.py --cfg configs/GatedGCN/MVN5-peptides-struct-GatedGCN.yaml  wandb.use False
+python main.py --cfg configs/GatedGCN/MVN5-peptides-struct-GatedGCN+RWSE.yaml  wandb.use False
 
-python main.py --cfg configs/GCN/MNV-peptides-func-GCN.yaml  wandb.use False
-python main.py --cfg configs/GCN/MVN-peptides-struct-GCN.yaml  wandb.use False
+python main.py --cfg configs/GCN/MNV5-peptides-func-GCN.yaml  wandb.use False
+python main.py --cfg configs/GCN/MVN5-peptides-struct-GCN.yaml  wandb.use False
 
-python main.py --cfg configs/GINE/MVN-peptides-func-GINE.yaml  wandb.use False
-python main.py --cfg configs/GINE/MVN-peptides-struct-GINE.yaml  wandb.use False
+python main.py --cfg configs/GINE/MVN5-peptides-func-GINE.yaml  wandb.use False
+python main.py --cfg configs/GINE/MVN5-peptides-struct-GINE.yaml  wandb.use False
+
+#MVN 10 nodes
+python main.py --cfg configs/GatedGCN/MVN10-peptides-func-GatedGCN.yaml  wandb.use False
+python main.py --cfg configs/GatedGCN/MVN10-peptides-func-GatedGCN+RWSE.yaml  wandb.use False
+
+python main.py --cfg configs/GatedGCN/MVN10-peptides-struct-GatedGCN.yaml  wandb.use False
+python main.py --cfg configs/GatedGCN/MVN10-peptides-struct-GatedGCN+RWSE.yaml  wandb.use False
+
+python main.py --cfg configs/GCN/MNV10-peptides-func-GCN.yaml  wandb.use False
+python main.py --cfg configs/GCN/MVN10-peptides-struct-GCN.yaml  wandb.use False
+
+python main.py --cfg configs/GINE/MVN10-peptides-func-GINE.yaml  wandb.use False
+python main.py --cfg configs/GINE/MVN10-peptides-struct-GINE.yaml  wandb.use False
 
 #HSG
 python main.py --cfg configs/GatedGCN/HSG-peptides-func-GatedGCN.yaml  wandb.use False
